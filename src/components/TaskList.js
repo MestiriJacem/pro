2119bac90152259850af920f2fbccabb99f7d056
MestiriJacem <mestirijacemm@gmail.com>
2025-01-19 11:38:22 +0100

    home

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import TaskItem from './TaskItem'
 
-const TaskList = () => {
+function TaskList ({tasks,toggleTaskCompletion,deleteTask})  {
   return (
-    <div>
-      
-    </div>
+    <ul className='task-list'>
+      {tasks.map((task)=>(
+        <TaskItem
+        key={task.id}
+        task={task}
+        toggleTaskCompletion={toggleTaskCompletion}
+        deleteTask={deleteTask}
+        />
+      ))}
+
+    </ul>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Home = () => {
+function Home ()  {
+  const [tasks,setTasks]=useState([]);
+  //load tasks from local storage
+  useEffect(()=>{
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks);
+  },[]
+  )
+  //save tasks to  local storage
+useEffect(()=>{
+  localStorage.setItem('tasks',JSON.stringify(tasks));
+},[tasks]
+)
+const addTask =(taskText)=>{
+  const newTask= {
+    id:Date.now(),text:taskText,completed:false
+  }
+  setTasks([...tasks,newTask])
+}
+const toggleTaskCompletion =(taskId)=>{
+  setTasks(tasks.map(task=>
+    task.id===taskId ? {...task,completed: !task.completed}:task
+  ))
+  const 
+}
+
   return (
     <div>
       home
