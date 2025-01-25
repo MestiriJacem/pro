fc9d20f18e85b4f57152bac0626715ae3f62e86f
MestiriJacem <mestirijacemm@gmail.com>
2025-01-25 12:47:36 +0100

    task item

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -11,12 +11,22 @@ function  TaskItem  ({task,toggleTask,deleteTask,editTask}) {
   }
   return (
     <li className={`task-item ${task.completed ? 'completed' : ''}`}>
-      <span onClick={()=>toggleTask(task.id)}>
- {task.text}
-      </span>
-      <button onClick={()=>deleteTask(task.id)}>❌ </button>
-    </li>
-  )
-}
-
+      {isEditing?(
+        <>
+        <input
+        type='text'
+        value={newText}
+        onChange={ (e)=>setNewText(e.target.value)}
+        className='edit-input'
+        
+        />
+        </>)
+        :( <>
+        <span onClick={()=> toggleTask(task.id)}>{task.text} </span>
+        <button onClick={()=> setIsEditing(true)} className='edit-btn'> edit</button>
+        <button onClick={()=> deleteTask(task.id)} className='delete-btn'> delete</button>
+        
+        </>)
+    }
+    
 export default TaskItem
